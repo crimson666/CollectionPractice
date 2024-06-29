beb083d67d762556584238649a277ee7d0860aa1
crimson666 <ankurbsws@gmail.com>
2024-06-29 09:22:59 +0530

    Stuff

--- src/CoolStuff/NetworkStuff.java
+++ src/CoolStuff/NetworkStuff.java
@@ -11,7 +11,11 @@ public class NetworkStuff {
         InetAddress inetAddress = Inet4Address.getByName(URL);
         System.out.println("Address : " + Arrays.toString(inetAddress.getAddress()));
         System.out.println("Host Address : " + inetAddress.getHostAddress());
-        System.out.println();
+        System.out.println("Local Address : " + inetAddress.isAnyLocalAddress());
+        System.out.println("Link local Address : " + inetAddress.isLinkLocalAddress());
+        System.out.println("Loop Back Address : " + inetAddress.isLoopbackAddress());
+        System.out.println("Site Local address : " + inetAddress.isSiteLocalAddress());
+        System.out.println("Hash code : " + inetAddress.hashCode());
 
     }
 }
